fb723b6179df6db9edb26dce39b3f52e08f3e189
dan <dan@46e82423-29d8-e211-989e-002590a4cdd4>
2022-09-11 20:25:20 +0000

    Let's move towards Nagios alerts instead of email.

--- compare.sh
+++ compare.sh
@@ -5,37 +5,43 @@
 # compare the commits on two different websites
 #
 
-if [ ! -f config.sh ]
+CONFIG=$1
+if [ ! -f $1 ]
 then
         echo "config.sh not found..."
         exit 1
 fi
 
-. config.sh
+. $1
+
+result=0
 
 fetch -qo source1.txt $SOURCE1 2>&1 > /dev/null
 fetch -qo source2.txt $SOURCE2 2>&1 > /dev/null
 
-diff source1.txt source2.txt > diff.txt
+diff -ruN source1.txt source2.txt > diff.txt
 
 NUMLINES=`cat diff.txt | wc -l`
-echo $NUMLINES
-echo "NUMLINES='$NUMLINES'"
+#echo $NUMLINES
+#echo "NUMLINES='$NUMLINES'"
 if [ $NUMLINES -gt 0 ]
 then
 	FIRSTLINE=`head -1 diff.txt`
 	THIRDLINE=`head -3 diff.txt | tail -1`
-	echo "\$FIRSTLINE='$FIRSTLINE'"
-	echo "\$THIRDLINE='$THIRDLINE'"
 	#
 	# no sense telling us that they are out by just one line.
 	# that is usually just timing.
 	#
 	if [   "$FIRSTLINE" != "1d0"  -a  "$FIRSTLINE" != "0a1"  -o  "$THIRDLINE" != '150a150'  -a "$THIRDLINE" != '150d150' ]
 	then
-		echo 'emailing'
-		cat diff.txt | mail -s "${SUBJECT}" $MAILTO
+		# not a trivial diff
+		echo diff.txt
+		result=1
 	else
-		echo 'ignoring'
+		# ignoring
 	fi
+else
+	echo "Same commits found"
 fi
+
+exit $result
